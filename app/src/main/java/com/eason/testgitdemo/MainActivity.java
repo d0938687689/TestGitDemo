c092757876ec8532c3058f7a3d59fb8778a8cb83
eason <d0938687689@gmail.com>
2018-09-17 20:47:04 +0800

    更新說明... Layout新增按鈕 透過點擊按鈕來計次，顯示次數在TextView上

--- app/src/main/java/com/eason/testgitdemo/MainActivity.java
+++ app/src/main/java/com/eason/testgitdemo/MainActivity.java
@@ -2,12 +2,22 @@ package com.eason.testgitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvResult;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvResult= findViewById(R.id.tvResult);
+    }
+
+    public void testClick(View view) {
+        count++;
+        tvResult.setText(count+"");
     }
 }
